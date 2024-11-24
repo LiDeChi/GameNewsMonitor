e0590c3036ce0bb409a798768464e917ea939d57
Parson <parsonjian@163.com>
2024-11-24 19:25:22 +0800

    Update setup script with absolute paths for conda and pip commands

--- deploy/setup.sh
+++ deploy/setup.sh
@@ -9,28 +9,31 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O mi
 bash miniconda.sh -b -p $HOME/miniconda
 rm miniconda.sh
 
-# 初始化conda
-$HOME/miniconda/bin/conda init bash
-source ~/.bashrc
+# 设置PATH以使用conda命令
+export PATH="$HOME/miniconda/bin:$PATH"
+
+# 初始化conda（但不修改.bashrc）
+conda init bash
+exec bash -l
 
 # 创建新的conda环境
-conda create -y -n gamenews python=3.10
+$HOME/miniconda/bin/conda create -y -n gamenews python=3.10
 
 # 激活环境
-conda activate gamenews
+source $HOME/miniconda/bin/activate gamenews
 
 # 安装基本依赖
-conda install -y numpy pandas beautifulsoup4 matplotlib seaborn
+$HOME/miniconda/bin/conda install -y numpy pandas beautifulsoup4 matplotlib seaborn
 
 # 安装其他依赖
-pip install playwright
-pip install python-dotenv
-pip install requests
-pip install fake-useragent
-pip install jieba
+$HOME/miniconda/envs/gamenews/bin/pip install playwright
+$HOME/miniconda/envs/gamenews/bin/pip install python-dotenv
+$HOME/miniconda/envs/gamenews/bin/pip install requests
+$HOME/miniconda/envs/gamenews/bin/pip install fake-useragent
+$HOME/miniconda/envs/gamenews/bin/pip install jieba
 
 # 安装playwright浏览器
-playwright install chromium
+$HOME/miniconda/envs/gamenews/bin/playwright install chromium
 
 # 确保在正确的目录
 cd ~/GameNewsMonitor
